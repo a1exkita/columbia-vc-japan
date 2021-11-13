72a3456f9f9a511128f82ee467ec1120ffea86db
ktsht <kitaa@oregonstate.edu>
2021-11-13 11:41:35 -0500

    Modified partner.js

--- pages/partners.js
+++ pages/partners.js
@@ -1,4 +1,5 @@
 import { gql } from "graphql-request";
+import { useState } from "react";
 import { graphcms } from "../lib/_graphcms";
 import Navbar from "../components/Navbar";
 import Head from "next/head";
@@ -10,6 +11,7 @@ const enum_type = {
 };
 
 export default function Partners({ data }) {
+    const [state, setstate] = useState([]);
     return (
         <div className="flex flex-col bg-black min-h-screen w-full">
             <Head>
@@ -20,22 +22,32 @@ export default function Partners({ data }) {
             <h1 className="font-serif font-extrabold text-5xl ml-8 mt-32 text-white">
                 Partners
             </h1>
-            <h5 className="font-sans font-medium text-sm mt-4 ml-8 w-5/12 text-gray-text">
-                Together with the following organizations, we work to foster a community-oriented approach to entrepreneurship and innovation for the betterment of the Columbia and entrepreneurial community in Japan.
+            <h5 className="font-sans font-medium text-sm mt-4 ml-8 w-7/12 text-gray-text">
+                Together with the following organizations, we work to foster a
+                community-oriented approach to entrepreneurship and innovation
+                for the betterment of the Columbia and entrepreneurial community
+                in Japan.
             </h5>
             <div className="grid grid-cols-3 gap-4 px-32 text-white mt-16">
-                {data.companies.map((company) => (
-                    <div key={company.id}>
-                        <div className="flex flex-col items-center mx-8">
-                            <img
-                                className="h-48 w-auto object-cover"
-                                src={company.logo.url}
-                                alt="image"
-                            />
-                            <p className="font-serif text-2xl text-white mt-4">
-                                {company.name}
-                            </p>
-                            {/* <div className="flex flex-col w-1/2">
+                {data.companies.map(function (company) {
+                    return (
+                        <div key={company.id}>
+                            <a href={company.link}>
+                                <div className="flex flex-col items-center mx-8">
+                                    <div className="flex flex-col justify-center h-48">
+                                        <img
+                                            className="h-auto w-full object-fit"
+                                            src={company.logo.url}
+                                            alt="image"
+                                        />
+                                    </div>
+                                    <p className="font-serif text-xl text-white mt-8 hover:underline">
+                                        {company.name}
+                                    </p>
+                                    <p className="my-4 font-sans fonts-medium text-xs w-full mt-8 text-center">
+                                        {company.description}
+                                    </p>
+                                    {/* <div className="flex flex-col w-1/2">
                                 <div className="flex flex-col justify-between w-7/12 pt-4 border-t-2 border-white">
                                     <a href={company.link}>
                                         <h1 className="text-white font-serif text-4xl w-full hover:underline">
@@ -47,9 +59,11 @@ export default function Partners({ data }) {
                                     </p>
                                 </div>
                             </div> */}
+                                </div>
+                            </a>
                         </div>
-                    </div>
-                ))}
+                    );
+                })}
             </div>
         </div>
     );
